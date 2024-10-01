6f9649c7552a5429108f659f46cdbc86f3e9833d
DongHoonYu96 <sbna154@gmail.com>
2024-10-01 20:45:24 +0900

    fix : static serving middleware fix

--- src/main/middlewares/middlewares.ts
+++ src/main/middlewares/middlewares.ts
@@ -1,10 +1,32 @@
 import {Request} from "../was/request";
 import {Response} from "../was/response";
-import {randomUUID} from "node:crypto";
+import * as path from "node:path";
+import * as fs from "node:fs";
 
 
-export const error404 = (err:Error, req : Request, res: Response, next) => {
-    res.status(404);
-    res.send("Not Found");
+export const staticServe = (req : Request, res : Response) => {
+    const mimeType = {
+        ".ico": "image/x-icon",
+        ".html": "text/html",
+        ".js": "text/javascript",
+        ".css": "text/css",
+        ".png": "image/png",
+        ".jpg": "image/jpeg",
+        ".eot": "appliaction/vnd.ms-fontobject",
+        ".ttf": "aplication/font-sfnt",
+    }
+    const ext = path.parse(req.path).ext
+    const publicPath = path.join(process.cwd(), 'dist','views');
+
+    if (Object.keys(mimeType).includes(ext)) {
+        fs.readFile(`${publicPath}${req.path}`, (err, data) => {
+            if (err) {
+                res.status(404).send();
+                return;
+            }
+
+            res.status(200).render(data);
+        })
+        return;
+    }
 }
-
