1e306dc53806474432b50538aded27f9b0bf704e
DongHoonYu96 <sbna154@gmail.com>
2024-10-03 00:47:03 +0900

    feat : 회원리스트 컨트롤러 구현
    ejs, 저장소에서 멤버get

--- src/main/frontcontroller/v3/controller/MemberListControllerV3.ts
+++ src/main/frontcontroller/v3/controller/MemberListControllerV3.ts
@@ -1,5 +1,6 @@
 import {ControllerV3} from "../ControllerV3";
 import {ModelView} from "../../ModelView";
+import {MemberRepository} from "../../../domain/member/MemberRepository";
 
 interface Member {
     id: number;
@@ -8,13 +9,12 @@ interface Member {
 }
 
 export class MemberListControllerV3 implements ControllerV3{
+
+    private memberRepository: MemberRepository = MemberRepository.getInstance();
+
     process(paramMap: Map<string, string>): ModelView {
-        //todo : repository에서 member 모두 찾아서 넣기
-        const members: Member[] =  [
-            { id: 1, loginId: 'user1', name: '홍길동' },
-            { id: 2, loginId: 'user2', name: '김철수' },
-            { id: 3, loginId: 'nvda', name: '젠슨황' }
-        ];
+        const members = this.memberRepository.findAll();
+
         const mv : ModelView = new ModelView("members");
         mv.getModel().set("members",members);
 
