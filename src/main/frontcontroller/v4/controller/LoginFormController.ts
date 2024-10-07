d418107608d655ee47de1e070ec84a2a4131582f
DongHoonYu96 <sbna154@gmail.com>
2024-10-07 21:43:14 +0900

    feat : login-form 구현
    로그인 누르면 post 요청 보내기

--- src/main/frontcontroller/v4/controller/LoginFormController.ts
+++ src/main/frontcontroller/v4/controller/LoginFormController.ts
@@ -3,6 +3,6 @@ import {ControllerV4} from "../ControllerV4";
 export class LoginFormController implements ControllerV4{
 
     process(paramMap: Map<string, string>, model: Map<string, object>): string {
-        return "login";
+        return "login-form";
     }
 }
