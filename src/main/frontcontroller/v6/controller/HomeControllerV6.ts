18231cd1230da340a1188d4d0f5f375941c1d1ee
DongHoonYu96 <sbna154@gmail.com>
2024-10-11 00:05:52 +0900

    feat : index에 실제 게시글 표시 구현

--- src/main/frontcontroller/v6/controller/HomeControllerV6.ts
+++ src/main/frontcontroller/v6/controller/HomeControllerV6.ts
@@ -2,14 +2,21 @@ import {ControllerV6} from "../ControllerV6";
 import {Request} from "../../../was/request";
 import {Response} from "../../../was/response";
 import {SessionManager} from "../../../utils/SessionManager";
+import {PostRepository} from "../../../domain/post/PostRepository";
 
 export class HomeControllerV6 implements ControllerV6{
 
     private sessionMgr : SessionManager = SessionManager.getInstance();
+    private postRepository  = PostRepository.getInstance().getRepo();
 
     async process(req: Request, res: Response, paramMap: Map<string, string>, model: Map<string, object>) {
         const findCookieVal = this.sessionMgr.findCookie(req, this.sessionMgr.SESSION_COOKIE_NAME);
         const findMember = this.sessionMgr.findMemberByCookieVal(findCookieVal);
+
+        const posts = await this.postRepository.find({
+            relations: ['member'],
+        });
+        model.set("posts",posts);
 
         /**
          * 로그인이 된경우, 동적 렌더링 필요
@@ -18,7 +25,10 @@ export class HomeControllerV6 implements ControllerV6{
             model.set("member",findMember);
             return "index";
         }
-        return "index";
+        else{
+            return "index";
+        }
+
     }
 
     version6() {
