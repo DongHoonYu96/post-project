c41176c365b31f401c8e546b1b00ece103459a29
DongHoonYu96 <sbna154@gmail.com>
2024-10-11 22:59:38 +0900

    docs : 컨트롤러v3,v4 주석추가

--- src/main/frontcontroller/v3/ControllerV3.ts
+++ src/main/frontcontroller/v3/ControllerV3.ts
@@ -1,5 +1,11 @@
 import {ModelView} from "../ModelView";
 
+/**
+ * 서블릿 종속성(res,req)을 제거한 버전의 컨트롤러
+ * 서블릿에 의존하지 않는 컨트롤러
+ * @param paramMap req.body를 파싱후 map에 넣은것
+ * @return ModelView 뷰이름과 렌더링할 객체들을 모델뷰 객체로 만들어 리턴
+ */
 export interface ControllerV3{
 
     process (paramMap : Map<string, string>) : ModelView;
