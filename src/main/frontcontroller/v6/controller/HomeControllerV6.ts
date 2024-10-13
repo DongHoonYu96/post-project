d5f1746048d3aaec6790a81bb63f0aba746057e8
DongHoonYu96 <sbna154@gmail.com>
2024-10-13 15:51:55 +0900

    feat : redis 초기설정

--- src/main/frontcontroller/v6/controller/HomeControllerV6.ts
+++ src/main/frontcontroller/v6/controller/HomeControllerV6.ts
@@ -3,34 +3,49 @@ import {Request} from "../../../was/request";
 import {Response} from "../../../was/response";
 import {SessionManager} from "../../../utils/SessionManager";
 import {PostRepository} from "../../../domain/post/PostRepository";
+import {RedisClient} from "../../../repositories/RedisClient";
 
 export class HomeControllerV6 implements ControllerV6{
 
     private sessionMgr : SessionManager = SessionManager.getInstance();
     private postRepository  = PostRepository.getInstance().getRepo();
+    private redisClient = RedisClient
 
     async process(req: Request, res: Response, paramMap: Map<string, string>, model: Map<string, object>) {
         const findCookieVal = this.sessionMgr.findCookie(req, this.sessionMgr.SESSION_COOKIE_NAME);
         const findMember = this.sessionMgr.findMemberByCookieVal(findCookieVal);
 
-        const posts = await this.postRepository.find({
-            relations: ['member'],
-        });
-        model.set("posts",posts);
+        // await this.redisClient.connect();
+
+        let posts;
+        const cachedPosts = await this.redisClient.get('posts');
+
+        if (cachedPosts) {
+            posts = JSON.parse(cachedPosts);
+        } else {
+            posts = await this.postRepository.find({
+                relations: ['member'],
+            });
+            await this.redisClient.set('posts', JSON.stringify(posts));
+        }
+        model.set("posts", posts);
 
         /**
          * 로그인이 된경우, 동적 렌더링 필요
          */
-        if(findMember){
-            model.set("member",findMember);
+        if (findMember) {
+            model.set("member", findMember);
+            return "index";
+        } else {
             return "index";
         }
-        else{
-            return "index";
-        }
-
     }
 
+
     version6() {
+    }
+
+    sayHello() {
+        //say hello
     }
 }
